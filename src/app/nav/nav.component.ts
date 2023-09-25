c3f983e5e86f25cad8a4379ed8ac55f5d0fcd705
ThinhNguyen9999 <100143580+ThinhNguyen9999@users.noreply.github.com>
2023-09-25 21:09:42 +0700

    Nav

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -9,5 +9,16 @@ import { ActivatedRoute, NavigationStart, Router, Event as NavigationEvent } fro
 export class NavComponent implements OnInit {
 
   ngOnInit() {
+    window.addEventListener('mouseup',function(event){
+      var drop = document.getElementById('pol') as HTMLElement;
+      if(event.target != drop && (<HTMLElement>event.target).parentNode != drop){
+        drop.style.display = 'none';
+      }
+    });
+  }
+
+  checkNav() {
+    var nav = document.getElementById('pol') as HTMLElement;
+    nav.style.display='block';
   }
 }
